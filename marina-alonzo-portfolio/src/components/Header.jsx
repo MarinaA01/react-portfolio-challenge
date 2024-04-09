3e3c6080f28d146f63963cdca071f831a6e10fbd
Marina <marinajalonzo1@gmail.com>
2024-04-08 19:35:04 -0500

    Layout

--- marina-alonzo-portfolio/src/components/Header.jsx
+++ marina-alonzo-portfolio/src/components/Header.jsx
@@ -6,7 +6,6 @@ function Header() {
         <header id="header">
             <div className="header">
                 <h1>Marina Alonzo</h1>
-                <h2>Full Stack Developer</h2>
             </div>
             <div className="nav">
                 <Nav />
